5f80c58f5a3a5cfe654ed581870f7612df4366f5
Fletcher Nichol <fnichol@nichol.ca>
2011-04-30 00:40:36 -0600

    Bump to v0.3.0.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures simple platform_packages without fuss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3"
+version          "0.3.0"
 recipe           "platform_packages", "Installs a set of simple packages from the platform's package manager"
 
 %w{ ubuntu debian arch suse }.each do |os|
